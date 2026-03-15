921189343886bf6ef0aa2321377d1a712205926c
emirhaanss <emrhn.turann@gmail.com>
2026-03-15 13:28:24 +0300

    Son 2 Tur Listelendi

--- Tourio/ViewComponents/TourViewComponents/_TourRightSidebarComponentPartial.cs
+++ Tourio/ViewComponents/TourViewComponents/_TourRightSidebarComponentPartial.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tourio.Services.TourServices;
 
 namespace Tourio.ViewComponents.TourViewComponents
 {
     public class _TourRightSidebarComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly ITourService _tourService;
+
+        public _TourRightSidebarComponentPartial(ITourService tourService)
         {
-            return View();
+            _tourService = tourService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var tours = await _tourService.GetAllToursAsync();
+            var lastTwoTours = tours.TakeLast(2).ToList();
+
+            return View(lastTwoTours);
         }
     }
 }
